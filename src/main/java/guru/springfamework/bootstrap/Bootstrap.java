7d6a6ed53b839593ea6555e40f96095ec5d6e6fc
Adam Samek <borg166@gmail.com>
2020-11-17 17:21:28 -0500

    Created Get API for Customers

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -4,43 +4,73 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import guru.springfamework.domain.Category;
+import guru.springfamework.domain.Customer;
 import guru.springfamework.repositories.CategoryRepository;
+import guru.springfamework.repositories.CustomerRepository;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 	
 	private CategoryRepository categoryRepository;
+	private CustomerRepository customerRepository;
 
-	public Bootstrap(CategoryRepository categoryRepository) {
+
+
+	public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
 		this.categoryRepository = categoryRepository;
+		this.customerRepository = customerRepository;
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		Category fruits = new Category();
-		fruits.setName("Fruits");
-		
-		Category dried = new Category();
-		dried.setName("Dried");
+    @Override
+    public void run(String... args) throws Exception {
 
-		Category fresh = new Category();
-		fresh.setName("Fresh");
+        loadCategories();
+        loadCustomers();
+    }
 
-		Category exotic = new Category();
-		exotic.setName("Exotic");
+    private void loadCategories() {
+        Category fruits = new Category();
+        fruits.setName("Fruits");
 
-		Category nuts = new Category();
-		nuts.setName("Nuts");
-		
-		categoryRepository.save(fruits);
-		categoryRepository.save(dried);
-		categoryRepository.save(fresh);
-		categoryRepository.save(exotic);
-		categoryRepository.save(nuts);
-		
-		System.out.println("Data Loaded = " + categoryRepository.count());
-		
-	}
+        Category dried = new Category();
+        dried.setName("Dried");
+
+        Category fresh = new Category();
+        fresh.setName("Fresh");
+
+        Category exotic = new Category();
+        exotic.setName("Exotic");
+
+        Category nuts = new Category();
+        nuts.setName("Nuts");
+
+        categoryRepository.save(fruits);
+        categoryRepository.save(dried);
+        categoryRepository.save(fresh);
+        categoryRepository.save(exotic);
+        categoryRepository.save(nuts);
+
+        System.out.println("Categories Loaded: " + categoryRepository.count());
+    }
+
+    private void loadCustomers() {
+        //given
+        Customer customer1 = new Customer();
+        customer1.setId(1l);
+        customer1.setFirstname("Michale");
+        customer1.setLastname("Weston");
+        
+        customerRepository.save(customer1);
+
+        Customer customer2 = new Customer();
+        customer2.setId(2l);
+        customer2.setFirstname("Sam");
+        customer2.setLastname("Axe");
+
+        customerRepository.save(customer2);
+
+        System.out.println("Customers Loaded: " + customerRepository.count());
+    }
 	
 	
 
